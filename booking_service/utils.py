b7e50f36c0bfacb433d2d21c1812752e3c4cef2a
vlkttlv <vitavitavita04@bk.ru>
2025-02-28 15:02:31 +0700

    add utils

---
+++ booking_service/utils.py
@@ -0,0 +1,10 @@
+from fastapi import HTTPException, Request
+import requests
+
+def check_current_user(request: Request):
+    access_token = request.cookies.get("booking_access_token")
+    headers = {'accept': 'application/json', 'token': access_token}
+    user_response = requests.get('http://127.0.0.1:8000/auth/me', headers=headers)
+    if user_response.status_code == 401:
+        raise HTTPException(status_code=401, detail="Not authorized")
+    return user_response.json()
