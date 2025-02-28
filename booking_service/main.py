dabceec193724c4cc77f0e343a14157b9b7f2493
vlkttlv <vitavitavita04@bk.ru>
2025-02-28 15:02:50 +0700

    add cors

--- booking_service/main.py
+++ booking_service/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from booking_service.router import router as booking_router
 from booking_service.router import api_router as api_booking_router
 
 app = FastAPI(title="Booking Microservice")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000", "http://localhost:8001", "http://localhost:8003"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(booking_router)
 app.include_router(api_booking_router)
+
